ac60040090612432d987e377c048df5619fdf6b3
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-01-06 15:09:32 +0800

    module: refactor

--- customize.sh
+++ customize.sh
@@ -3,11 +3,14 @@
 
 # unmount older module
 umount -l /vendor/etc
-mkdir -p $MODPATH/vendor/etc/selinux
-grep -v "lineage" /vendor/etc/selinux/vendor_sepolicy.cil > $MODPATH/vendor/etc/selinux/vendor_sepolicy.cil
-susfs_clone_perm $MODPATH/vendor/etc/selinux/vendor_sepolicy.cil /vendor/etc/selinux/vendor_sepolicy.cil
+mkdir -p $MODPATH/overlay/vendor/etc/selinux
+grep -v "lineage" /vendor/etc/selinux/vendor_sepolicy.cil > $MODPATH/overlay/vendor/etc/selinux/vendor_sepolicy.cil
+susfs_clone_perm $MODPATH/overlay/vendor/etc/selinux/vendor_sepolicy.cil /vendor/etc/selinux/vendor_sepolicy.cil
 
 umount -l /system/etc
-mkdir -p $MODPATH/system/etc/vintf
-grep -v "lineage" /system/etc/vintf/compatibility_matrix.device.xml > $MODPATH/system/etc/vintf/compatibility_matrix.device.xml
-susfs_clone_perm $MODPATH/system/etc/vintf/compatibility_matrix.device.xml /system/etc/vintf/compatibility_matrix.device.xml
+mkdir -p $MODPATH/overlay/system/etc/vintf
+grep -v "lineage" /system/etc/vintf/compatibility_matrix.device.xml > $MODPATH/overlay/system/etc/vintf/compatibility_matrix.device.xml
+susfs_clone_perm $MODPATH/overlay/system/etc/vintf/compatibility_matrix.device.xml /system/etc/vintf/compatibility_matrix.device.xml
+
+# skip mount
+touch $MODPATH/skip_mount
--- service.sh
+++ service.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 MODDIR="${0%/*}"
-source ${MODDIR}/utils.sh
+. $MODDIR/utils.sh
 
-# make sure ${MODDIR}/skip_mount exists!
-# this way ksu wont mount it
-# we handle the mounting ourselves
-[ ! -f ${MODDIR}/skip_mount ] && touch ${MODDIR}/skip_mount
+# wait for boot complete to avoid android system screaming at us
+until [ "$(getprop sys.boot_completed)" == "1" ]; do
+    sleep 1
+done
 
 modid="my_vendor"
 
@@ -15,10 +15,10 @@ modid="my_vendor"
 # we mimic vendor mounts like, my_bigball
 mkdir $basefolder/$modid
 
-cd $MODDIR
+cd $MODDIR/overlay
 
 for i in $(ls -d */*); do
 	mkdir -p $basefolder/$modid/$i
-	mount --bind $MODDIR/$i $basefolder/$modid/$i
+	mount --bind $MODDIR/overlay/$i $basefolder/$modid/$i
 	mount -t overlay -o "lowerdir=$basefolder/$modid/$i:/$i" overlay /$i
 done
