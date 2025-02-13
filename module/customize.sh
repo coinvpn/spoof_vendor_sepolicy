81bdf98f31813a3fadf2eeca5567afc6e6278cfb
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-02-13 18:11:27 +0800

    scripts/customize: create fake files at install

--- module/customize.sh
+++ module/customize.sh
@@ -1,11 +1,45 @@
 #!/bin/sh
+. $MODPATH/utils.sh
 
-# overlayfs check
-if ! grep -q overlay /proc/filesystems > /dev/null 2>&1; then \
-	abort "[!] OverlayFS is required for this module!"
+echo "[+] mountify"
+echo "[+] SysReq test"
+# test for overlayfs
+if grep -q "overlay" /proc/filesystems > /dev/null 2>&1; then \
+	echo "[+] CONFIG_OVERLAY_FS"
+	echo "[+] overlay found in /proc/filesystems"
+else
+	abort "[!] CONFIG_OVERLAY_FS is required for this module!"
 fi
+
+# test for tmpfs xattr
+[ -w /mnt ] && MNT_FOLDER=/mnt
+[ -w /mnt/vendor ] && MNT_FOLDER=/mnt/vendor
+testfile="$MNT_FOLDER/tmpfs_xattr_testfile"
+rm $testfile > /dev/null 2>&1 
+busybox mknod "$testfile" c 0 0 > /dev/null 2>&1 
+if busybox setfattr -n trusted.overlay.whiteout -v y "$testfile" > /dev/null 2>&1 ; then 
+	echo "[+] CONFIG_TMPFS_XATTR"
+	echo "[+] tmpfs extended attribute test passed"
+else
+	abort "[!] CONFIG_TMPFS_XATTR is required for this module!"
+fi
+rm $testfile > /dev/null 2>&1 
 
 # skip mount
 touch $MODPATH/skip_mount
+# mountify 131+
+touch $MODPATH/skip_mountify
+
+
+echo "[+] creating sepolicy.cil"
+# create spoofed files
+mkdir -p "$MODPATH/system/vendor/etc/selinux"
+grep -v "lineage" /vendor/etc/selinux/vendor_sepolicy.cil > "$MODPATH/system/vendor/etc/selinux/vendor_sepolicy.cil"
+susfs_clone_perm "$MODPATH/system/vendor/etc/selinux/vendor_sepolicy.cil" /vendor/etc/selinux/vendor_sepolicy.cil
+
+echo "[+] creating compatibility_matrix.device.xml"
+mkdir -p "$MODPATH/system/etc/vintf"
+grep -v "lineage" /system/etc/vintf/compatibility_matrix.device.xml > "$MODPATH/system/etc/vintf/compatibility_matrix.device.xml"
+susfs_clone_perm "$MODPATH/system/etc/vintf/compatibility_matrix.device.xml" /system/etc/vintf/compatibility_matrix.device.xml
 
 # EOF
--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -1,25 +1,7 @@
 #!/bin/sh
 PATH=/data/adb/ap/bin:/data/adb/ksu/bin:/data/adb/magisk:$PATH
 MODDIR=${0%/*}
-. $MODDIR/utils.sh
 
-[ -w /sbin ] && TMPFS_DIR="/sbin/spoof_vendor_sepolicy"
-[ -w /debug_ramdisk ] && TMPFS_DIR="/debug_ramdisk/spoof_vendor_sepolicy"
-
-mkdir -p "$TMPFS_DIR/vendor/etc/selinux"
-grep -v "lineage" /vendor/etc/selinux/vendor_sepolicy.cil > "$TMPFS_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
-susfs_clone_perm "$TMPFS_DIR/vendor/etc/selinux/vendor_sepolicy.cil" /vendor/etc/selinux/vendor_sepolicy.cil
-
-mkdir -p "$TMPFS_DIR/system/etc/vintf"
-grep -v "lineage" /system/etc/vintf/compatibility_matrix.device.xml > "$TMPFS_DIR/system/etc/vintf/compatibility_matrix.device.xml"
-susfs_clone_perm "$TMPFS_DIR/system/etc/vintf/compatibility_matrix.device.xml" /system/etc/vintf/compatibility_matrix.device.xml
-
-# fake /system/bin/service output
-# will work as long as they dont check binary size
-mkdir -p "$TMPFS_DIR/system/bin"
-cp /system/bin/service "$TMPFS_DIR/system/bin/service.orig"
-susfs_clone_perm "$TMPFS_DIR/system/bin/service.orig" /system/bin/service
-echo "IyEvYmluL3NoCi9zeXN0ZW0vYmluL3NlcnZpY2Uub3JpZyAiJEAiIHwgc2VkICdzL2xpbmVhZ2UvL2c7IHMvTGluZWFnZS8vZycK" | base64 -d > "$TMPFS_DIR/system/bin/service"
-susfs_clone_perm "$TMPFS_DIR/system/bin/service" /system/bin/service
+# do nothing 
 
 # EOF
