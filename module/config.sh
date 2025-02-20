13001807e39f2b9485e5694cedf1c158ea3304bc
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-02-20 19:59:47 +0800

    scripts/post-fs-data: import mountify standalone

---
+++ module/config.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# config.sh
+# No warranty.
+# No rights reserved.
+# This is free software; you can redistribute it and/or modify it under the terms of The Unlicense.
+FAKE_MOUNT_NAME="my_vendor_sepolicy"
+
+# EOF
